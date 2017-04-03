4059863f862034dc2c0e654e47af66e7fc3d38eb
Tim <tim.vdh@gmail.com>
2017-04-03 14:25:31 +1200

    add count-external endpoint

--- src/endpoints/v1/events.js
+++ src/endpoints/v1/events.js
@@ -22,7 +22,7 @@ class Events extends BaseEndpoint {
         super(client)
 
         this.schema = EventSchema
-        this.arrayOptions = ['category', 'sort', 'top_events.sort', 'rank_level', 'label', 'country','place.scope','place.exact']
+        this.arrayOptions = ['category', 'sort', 'top_events.sort', 'rank_level', 'label', 'country','place.scope','place.exact', 'external_id']
         this.integerOptions = ['limit', 'offset', 'rank_level']
 
         this.accountId = accountId
@@ -37,6 +37,22 @@ class Events extends BaseEndpoint {
 
         if (validate.valid) {
             return this.get('v1', '/events/', EventResultSet, options)
+        }
+
+        return new Promise((resolve, reject) => {
+            return reject(validate.errors[0])
+        })
+
+    }
+
+    countExternal(options){
+
+        options = options || {}
+
+        let validate = this.validate(options)
+
+        if (validate.valid) {
+            return this.get('v1', '/events/count-external/', EventCountResult, options)
         }
 
         return new Promise((resolve, reject) => {
