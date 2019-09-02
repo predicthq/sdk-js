05af0ff4dd601237b9f0e8fe2ff3cea8ef3ed260
Matthieu Bonnefoy <matthieu@predicthq.com>
2019-09-02 12:31:57 +1200

    Update the tests

--- test/test_events_search.js
+++ test/test_events_search.js
@@ -159,22 +159,6 @@ describe('Events', () => {
 
     }),
 
-    it('Search using account', (done) => {
-
-        let c = new Client({access_token: test_user_password_access_token})
-
-        c.events.for_account(test_account_id).search()
-            .then((results)=> {
-
-                // default limit 10 records
-                expect(results.toArray().length).toEqual(10)
-
-                done()
-
-            }).catch(done)
-
-    }),
-
     it('Calendar', (done) => {
 
         let c = new Client({access_token: test_client_credentials_access_token})
@@ -198,29 +182,4 @@ describe('Events', () => {
             }).catch(done)
 
     })
-
-    it('Calendar using Account', (done) => {
-
-        let c = new Client({access_token: test_user_password_access_token})
-
-        let options = {
-            'active.gte': '2016-01-01',
-            'active.lte': '2016-01-03',
-            'country': 'CA',
-            'top_events.limit': 1,
-            'top_events.sort': ['rank']
-        }
-
-        c.events.for_account(test_account_id).calendar(options)
-            .then((results)=> {
-
-                let days = _.keyBy(results.toArray(), 'date')
-                expect(_.keys(days)).toEqual(['2016-01-01', '2016-01-02', '2016-01-03'])
-
-                done()
-
-            }).catch(done)
-
-    })
-
 })
